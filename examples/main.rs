a21c0ae8805bc45c758a3a147041221bb9715189
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-11-13 18:34:14 -0500

    move test strategy to examples

--- examples/main.rs
+++ examples/main.rs
@@ -1,4 +1,18 @@
-use strategy_runner::StrategyRunnerBuilder;
+use strategy_runner::{Error, Event, StrategyFn, StrategyRunnerBuilder, StrategyState, Triggers};
+
+struct TestStrategy;
+
+impl StrategyFn for TestStrategy {
+    fn process(&mut self, triggers: Triggers, state: &StrategyState) -> Result<(), Error> {
+        println!("TestStrategy::process");
+        Ok(())
+    }
+
+    fn triggers(&self) -> Triggers {
+        Event::BtcUpdate | Event::EthUpdate
+    }
+}
+
 fn main() {
     let runner = StrategyRunnerBuilder::new()
         .add_exchange(todo!())
